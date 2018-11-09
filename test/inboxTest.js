bacd742cad655c8befd8a5a8db2f53bd39dfb7db
Elephant_Man <grogan.charlie@gmail.com>
2018-11-09 11:40:25 -0600

    successful launch of contract using web3

--- test/inboxTest.js
+++ test/inboxTest.js
@@ -2,17 +2,24 @@ const assert = require("chai").assert;
 const ganache = require("ganache-cli");
 const Web3 = require("web3"); // This imports the Constructor Web3
 const web3 = new Web3(ganache.provider()); // This creates the instance of web3 - attempt to local test network
+const { interface, bytecode } = require("../compile");
 
-beforeEach(() => {
+let accounts;
+let inbox;
+
+beforeEach(async () => {
   // Get a list of all accounts - solely for dev purposes - retrieves 10 test accounts
-  web3.eth.getAccounts().then(fetchedAccounts => {
-    console.log(fetchedAccounts);
-  });
+  accounts = await web3.eth.getAccounts();
 
   // Use one of those accounts to deploy
   // the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .send({ from: accounts[0], gas: "1000000" });
 });
 
 describe("Inbox", () => {
-  it("deploys a contract", () => {});
+  it("deploys a contract", () => {
+    console.log(inbox);
+  });
 });
